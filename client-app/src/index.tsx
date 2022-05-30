d94fd99b62fbc1fcefe96883c5240046079834ba
Genes Luna <genesluna@gmail.com>
2022-05-29 22:57:11 -0300

    Added react router

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
